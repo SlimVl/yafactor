3544b9ed752c1bf79b0da8e8d65414f03b5ac99a
SlimVl <55666453+SlimVl@users.noreply.github.com>
2023-02-22 15:36:59 +0500

    Update boot.php

--- config/boot.php
+++ config/boot.php
@@ -1,1 +1,26 @@
+<?php
 
+// Используем для подключения к базе данных MySQL
+class Database
+{
+    // Учётные данные базы данных
+    private $host = "localhost";
+    private $db_name = "yafactor";
+    private $username = "yafactor";
+    private $password = "aB8rY4pW5obM0u";
+    public $conn;
+
+    // Получаем соединение с базой данных
+    public function getConnection()
+    {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+        } catch (PDOException $exception) {
+            echo "Ошибка соединения с БД: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
